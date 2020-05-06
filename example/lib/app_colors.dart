a2d117b7d9bfb41ec28c1cbeccaaa889a8b9b35c
Daniel Ioannou <dioann28@gmail.com>
2020-05-06 18:08:44 +0300

    Update example

---
+++ example/lib/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const List<Color> avatarColors = [
+    Color(0xffe57373),
+    Color(0xfff06292),
+    Color(0xffba68c8),
+    Color(0xff9575cd),
+    Color(0xff7986cb),
+    Color(0xff64b5f6),
+    Color(0xff4fc3f7),
+    Color(0xff4dd0e1),
+    Color(0xff4db6ac),
+    Color(0xff81c784),
+    Color(0xffaed581),
+    Color(0xffff8a65),
+    Color(0xffd4e157),
+    Color(0xffffd54f),
+    Color(0xffffb74d),
+    Color(0xffa1887f),
+    Color(0xff90a4ae)
+  ];
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,62 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:group_list_view/group_list_view.dart';
+import 'package:group_listview_example/app_colors.dart';
 
 void main() => runApp(MyApp());
+
+Map<String, List> _elements = {
+  'Team A': ['Klay Lewis', 'Ehsan Woodard', 'River Bains'],
+  'Team B': ['Toyah Downs', 'Tyla Kane'],
+  'Team C': ['Marcus Romero', 'Farrah Parkes', 'Fay Lawson', 'Asif Mckay'],
+  'Team D': [
+    'Casey Zuniga',
+    'Ayisha Burn',
+    'Josie Hayden',
+    'Kenan Walls',
+    'Mario Powers'
+  ],
+  'Team Q': ['Toyah Downs', 'Tyla Kane', 'Toyah Downs'],
+};
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Group List View Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Group List View Demo'),
+        ),
+        body: GroupListView(
+          sectionsCount: _elements.keys.toList().length,
+          countOfItemInSection: (int section) {
+            return _elements.values.toList()[section].length;
+          },
+          itemBuilder: _itemBuilder,
+          groupHeaderBuilder: (BuildContext context, int section) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+              child: Text(
+                _elements.keys.toList()[section],
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              ),
+            );
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+    );
+  }
+
+  Widget _itemBuilder(BuildContext context, IndexPath index) {
+    String user = _elements.values.toList()[index.section][index.index];
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 5),
+      child: Card(
+        elevation: 8,
+        child: ListTile(
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 18, vertical: 10.0),
+          leading: CircleAvatar(
+            child: Text(
+              _getInitials(user),
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+            backgroundColor: _getAvatarColor(user),
+          ),
+          title: Text(
+            _elements.values.toList()[index.section][index.index],
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+          ),
+        ),
       ),
     );
+  }
+
+  String _getInitials(String user) {
+    var buffer = StringBuffer();
+    var split = user.split(" ");
+    for (var s in split) buffer.write(s[0]);
+
+    return buffer.toString().substring(0, split.length);
+  }
+
+  Color _getAvatarColor(String user) {
+    return AppColors
+        .avatarColors[(user?.hashCode ?? 1) % AppColors.avatarColors.length];
   }
 }
